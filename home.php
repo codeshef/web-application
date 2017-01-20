e99d6f73d36bb796fca70a07d26d89958f309928
codeshef <mukulanchal@gmail.com>
2017-01-20 23:16:57 +0530

    adding home.php

---
+++ home.php
@@ -0,0 +1,18 @@
+<?php
+  
+  session_start();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Registration</title>
+<link rel = "stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h1>Home</h1>
+<div> <h4>Welcome
+<?php  
+?></h4> </div>
+</body>
+</html>
