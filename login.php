8ec364ccbc25e58b8d8e87ee6a34fcf8746ec6fe
codeshef <mukulanchal@gmail.com>
2017-01-21 22:08:29 +0530

    adding $_SESSION['message'] feature

--- login.php
+++ login.php
@@ -1,8 +1,9 @@
 <?php
  //connect to database
+    session_start();
    $db = mysqli_connect("localhost","root","","apply");
    if(isset($_POST['submit'])!='')
-   {     session_start();  
+   {     //session_start();  
   
 	   $username = mysql_real_escape_string($_POST['username']);
 	   $password = mysql_real_escape_string($_POST['password']);
@@ -14,12 +15,12 @@
 			$result = mysqli_query($db,$sql);
 			$rows = mysqli_num_rows($result);
 			if($rows==1)
-			{echo "Congratulations! you have successfully logged in!<br>";
-			echo $username;
+			{ $_SESSION['message']="You are now logged in";
+			   $_SESSION['username']=$username;
 			header("Location:home.php");}
 			else
-			{
-				echo "Invalid username and password";
+			{   $_SESSION['message']="Invalid username and password";
+				
 			}
 	} 
 			
@@ -28,23 +29,31 @@
 <html>
 <head>
 <title>Login Page</title>
-<link rel = "stylesheet" type="text/css" href="style.css">
-</head>
+<link rel = "stylesheet" type="text/css" href="style1.css">
 <body>
+<?php
+ if(isset($_SESSION['message']))
+ {
+	 echo "<div id = 'frm'>".$_SESSION['message']."</div>";
+	 unset($_SESSION['message']);
+ }
+?>
+
 <div id ="frm">
 <h2>Sign in</h2>
 <form action="login.php" method="POST">
 <p>
 <p><label>Username</label></p>
-<input type ="text"  name="username" class="textInput"/>
+<input type ="text"  name="username" class="textInput" id="user"/>
 </p>
 <p>
 <p><label>Password</label></p>
-<input type ="password"  name="password" class="textInput"/>
+<input type ="password"  name="password" class="textInput" id="pass"/>
 </p>
 <p>
 <input type ="submit"  name="submit" value="submit" id="btn"/>
 </p>
 </form>
 </body>
+</head>
 </html>
